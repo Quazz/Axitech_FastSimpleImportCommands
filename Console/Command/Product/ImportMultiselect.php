be5909764dafbf72f40fe5e39e09957f6678559f
Quazz <nickverschragen@gmail.com>
2019-02-15 13:20:34 +0100

    Update ImportMultiselect.php

--- Console/Command/Product/ImportMultiselect.php
+++ Console/Command/Product/ImportMultiselect.php
@@ -3,8 +3,8 @@
  * Copyright © 2016 FireGento e.V. - All rights reserved.
  * See LICENSE.md bundled with this module for license details.
  */
-namespace FireGento\FastSimpleImportDemo\Console\Command\Product;
-use FireGento\FastSimpleImportDemo\Console\Command\AbstractImportCommand;
+namespace Axitech\FastSimpleImportCommands\Console\Command\Product;
+use Axitech\FastSimpleImportCommands\Console\Command\AbstractImportCommand;
 use Magento\Framework\App\ObjectManagerFactory;
 use Magento\ImportExport\Model\Import;
 use Magento\Catalog\Api\ProductAttributeOptionManagementInterface;
@@ -19,7 +19,7 @@ class ImportMultiselect extends AbstractImportCommand
 
     protected function configure()
     {
-        $this->setName('fastsimpleimportdemo:products:importmultiselect')
+        $this->setName('fastsimpleimportcommands:products:importmultiselect')
             ->setDescription('Import Simple Products with Multiselect ');
 
         $this->setBehavior(Import::BEHAVIOR_ADD_UPDATE);
