d0f96b240262de55960ffd0cd17feea742135b3c
Quazz <nickverschragen@gmail.com>
2019-02-15 13:24:17 +0100

    Update ImportCustomer.php

--- Console/Command/Customer/ImportCustomer.php
+++ Console/Command/Customer/ImportCustomer.php
@@ -3,9 +3,9 @@
  * Copyright © 2016 FireGento e.V. - All rights reserved.
  * See LICENSE.md bundled with this module for license details.
  */
-namespace FireGento\FastSimpleImportDemo\Console\Command\Customer;
+namespace Axitech\FastSimpleImportCommands\Console\Command\Customer;
 
-use FireGento\FastSimpleImportDemo\Console\Command\AbstractImportCommand;
+use Axitech\FastSimpleImportCommands\Console\Command\AbstractImportCommand;
 use Magento\ImportExport\Model\Import;
 
 /**
@@ -19,7 +19,7 @@ class ImportCustomer extends AbstractImportCommand
 
     protected function configure()
     {
-        $this->setName('fastsimpleimportdemo:customers:import')
+        $this->setName('fastsimpleimportcommands:customers:import')
             ->setDescription('Import Customer ');
 
         $this->setBehavior(Import::BEHAVIOR_APPEND);
