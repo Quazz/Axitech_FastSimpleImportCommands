63096edc5d59a768ef48175a09b9a6b5374df579
Quazz <nickverschragen@gmail.com>
2019-02-15 15:17:21 +0100

    Update ExportProducts.php
    Attempt to use Logistic module for export since SimpleImportExport does not provide this natively.

--- Console/Command/Product/ExportProducts.php
+++ Console/Command/Product/ExportProducts.php
@@ -6,8 +6,7 @@
  *
  */
 namespace Axitech\FastSimpleImportCommands\Console\Command\Product;
-use Axitech\FastSimpleImportCommands\Console\Command\AbstractExportCommand;
-use Axitech\FastSimpleImportCommands\Console\Command\AbstractImportCommand;
+use PH2M\Logistic\Model\Export\AbstractExport;
 use Magento\Framework\App\ObjectManagerFactory;
 use Magento\ImportExport\Model\Import;
 use Magento\Catalog\Api\ProductAttributeOptionManagementInterface;
@@ -16,15 +15,48 @@ use Magento\Catalog\Api\ProductAttributeOptionManagementInterface;
  * @package FireGento\FastSimpleImport2\Console\Command
  *
  */
-class ExportProducts extends AbstractExportCommand
+class ExportProducts extends PH2M\Logistic\Model\Export\AbstractExport
 {
 
+	/* Set create file per object to false */
+	protected $createAFileForEachObject = false;
+	/* Code variable */
+	protected $code = 'export';
+	/* Product collection factory */
+	protected $collectionFactory;
+	
+	public function __construct(
+    \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $collectionFactory
+) {
+    $this->collectionFactory = $collectionFactory;
+}
+	
+	/* Set filename to desired */
+	protected function _getFileName($object)
+    {
+        return 'ProductExport.csv';
+    }
+	
+	/* Generate full product collection */
+	protected function _generateProductCollection() 
+	{
+		$productCollection = $this->collectionFactory->create();
+		/** Apply filters here */
+		$productCollection->addAttributeToSelect('*');
+	
+		return $productCollection;
+	}
+	
+	/* Pass objects to export */
+	protected function _initObjectsToExport()
+    {
+        $this->objectsToExport = $this->_generateProductsCollection();
+    }
 
     protected function configure()
     {
         $this->setName('fastsimpleimportcommands:products:export')
-            ->setDescription('Export Products ');
-        $this->setEntityCode('catalog_product');
+            ->setDescription('Export Products');
 
         parent::configure();
     }
