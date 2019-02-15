8fa491598bb8c1c6d14cda4d48c80f5c57eccf49
Quazz <nickverschragen@gmail.com>
2019-02-15 13:29:42 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'FireGento_FastSimpleImportDemo',
+    'Axitech_FastSimpleImportCommands',
     __DIR__
 );
