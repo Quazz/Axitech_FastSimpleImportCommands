e6cdee9c919b43551ecb026bd40e3cf970429849
Quazz <nickverschragen@gmail.com>
2019-02-15 13:23:33 +0100

    Update ExportCustomer.php

--- Console/Command/Customer/ExportCustomer.php
+++ Console/Command/Customer/ExportCustomer.php
@@ -5,9 +5,9 @@
  *  * See LICENSE.md bundled with this module for license details.
  *
  */
-namespace FireGento\FastSimpleImportDemo\Console\Command\Customer;
+namespace Axitech\FastSimpleImportCommands\Console\Command\Customer;
 use Magento\ImportExport\Model\Import;
-use FireGento\FastSimpleImportDemo\Console\Command\AbstractExportCommand;
+use Axitech\FastSimpleImportCommands\Console\Command\AbstractExportCommand;
 /**
  * Class TestCommand
  * @package FireGento\FastSimpleImport2\Console\Command
@@ -19,7 +19,7 @@ class ExportCustomer extends AbstractExportCommand
 
     protected function configure()
     {
-        $this->setName('fastsimpleimportdemo:customers:export')
+        $this->setName('fastsimpleimportcommands:customers:export')
             ->setDescription('Export Customers');
         $this->setEntityCode('customer');
 
